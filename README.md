53e935976dc8a416bffb6a469c3d7b5a91d7f3a3
chelsea <chelsea@example.com>
2017-01-05 17:27:02 +0800

    change file1 to file2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # python some exercise
+
+keepalived-lvs01.conf,test.txt
+其中keepalive*此文件为原有文件
+test.txt文件为需要touch的文件
+
+当keepalived-lvs01.conf文件里面有东西有东西更改后，keepalived-lvs02.conf文件也需要同步lvs01的文件，file.py文件就可以保存一份为keepalived-lvs02.conf的文件（在此时为test.txt），然后用rsync就可以传到lvs02机器上，备份之前的文件，吧test.txt文件更名就可以了，这样可以更方便的更改文件内容，因为文件更改不是有序的改动，所以这样更快捷.
---
+++ file.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+f = file("keepalived-lvs01.conf")
+t = file("test.txt","r+")
+h = """! Configuration File for keepalived
+
+global_defs {
+   router_id LVS_G1_02
+}
+
+vrrp_sync_group VGKU {
+   group {
+      VI_READ_KU
+   }
+}
+
+
+vrrp_instance VI_READ_KU {
+    state BACKUP
+    interface bond0.192
+    virtual_router_id 40
+    priority 90
+    advert_int 1
+    authentication {
+        auth_type PASS
+        auth_pass lvs01
+    }
+"""
+for i in f.readlines()[21:]:
+    t.write( i )
+
+t = file("test.txt","r")
+content = t.read()
+pos = content.find("    virtual_ipaddress")
+if pos != -1:
+    content = content[:pos] + h +content[pos:]
+    t = file("test.txt","w")
+    t.write( content )
+    print "ok"
